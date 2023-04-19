334d84f265cf750e9c88f709025ce3603b10bec8
Eloy <usuari@domini.com>
2023-04-19 13:24:23 +0200

    usuaris2

---
+++ src/main/java/com/mycompany/banc/usuaris.java
@@ -0,0 +1,85 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.banc;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+
+
+/**
+ *
+ * @author alumne
+ */
+public class usuaris {
+
+    static void add(Usuario usuario) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+   public class Usuario {
+    private String username;
+    private String password;
+
+    public Usuario(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+    public class Usuarios {
+    private ArrayList<Usuario> usuarios;
+
+    public Usuarios() {
+        usuarios = new ArrayList<Usuario>();
+        usuarios.add(new Usuario("user1", "contraseña1"));
+        usuarios.add(new Usuario("user2", "contraseña2"));
+        usuarios.add(new Usuario("user3", "contraseña3"));
+        usuarios.add(new Usuario("user4", "contraseña4"));
+    }
+    public class Usuari {
+    private String username;
+    private String password;
+
+    public Usuari(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    // Métodos getters y setters
+}
+
+}
+
+     private void login() throws IOException {
+ App.setRoot("secondry1");
+}
+
+    }
+}
+    
+
+
+
+    
+
+   
+
+
+    
+ 
+
+    
+
+    
+
+
+
